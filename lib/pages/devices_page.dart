fea62ebbfc75af57d30facfdb4d16eeed07d35f2
Mihai-Tudor Popescu <mihai.tudor.popescu@gmail.com>
2022-11-20 10:57:08 +0200

    Fixed devices tab

--- lib/pages/devices_page.dart
+++ lib/pages/devices_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_settings_screens/flutter_settings_screens.dart';
 import 'package:safety_driving/widget/navigation_drawer_widget.dart';
 
 class DevicesPage extends StatefulWidget {
@@ -7,58 +8,28 @@ class DevicesPage extends StatefulWidget {
 }
 
 class _DevicesPageState extends State<DevicesPage> {
-  String _wearableDevice = 'No wearable device connected';
-  String _vehicle = 'No vehicle connected';
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: NavigationDrawerWidget(),
-      appBar: AppBar(
-        title: Text('Devices'),
-        centerTitle: true,
-        backgroundColor: Colors.lightBlue,
-      ),
-      body: Column(children: <Widget>[
-        Center(
-            child: OutlinedButton(
-          style: outlineButtonStyle,
-          onPressed: () {},
-          child: Text('Connect wearable'),
-        )),
-        Center(
-          child: OutlinedButton(
-              style: outlineButtonStyle,
-              onPressed: () {},
-              child: Text('Connect vehicle')),
+        drawer: NavigationDrawerWidget(),
+        appBar: AppBar(
+          title: const Text('Devices'),
+          centerTitle: true,
+          backgroundColor: Colors.lightBlue,
         ),
-        Expanded(
-          child: ListView(children: <Widget>[
-            buildDeviceItem(context, text: _wearableDevice),
-            buildDeviceItem(context, text: _vehicle)
-          ]),
-        )
-      ]),
-    );
+        body: Column(children: <Widget>[
+          Center(
+            child: SettingsGroup(
+              title: 'Connect devices',
+              children: [
+                TextInputSettingsTile(
+                  settingKey: 'wearable-device',
+                  title: 'Wearable device',
+                ),
+                TextInputSettingsTile(title: 'Vehicle', settingKey: 'vehicle')
+              ],
+            ),
+          )
+        ]));
   }
-
-  buildDeviceItem(BuildContext context, {required String text}) {
-    const textColor = Colors.black;
-
-    return Material(
-        color: Colors.transparent,
-        child: ListTile(
-            selectedTileColor: Colors.white24,
-            title: Text(text,
-                style: const TextStyle(color: textColor, fontSize: 20))));
-  }
-
-  final ButtonStyle outlineButtonStyle = OutlinedButton.styleFrom(
-    foregroundColor: Colors.black87,
-    minimumSize: const Size(200, 100),
-    padding: const EdgeInsets.symmetric(horizontal: 30),
-    shape: const RoundedRectangleBorder(
-      borderRadius: BorderRadius.all(Radius.circular(2)),
-    ),
-  );
 }
